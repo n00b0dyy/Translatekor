4314e045869190802c868328276fc79628ed23e4
Lukas <162102955+n00b0dyy@users.noreply.github.com>
2024-05-15 14:52:55 +0200

    Update clear_comtypes_cache.py

--- Scripts/clear_comtypes_cache.py
+++ Scripts/clear_comtypes_cache.py
@@ -1,5 +1,19 @@
 import os
 import shutil
+import sys
+
+def get_next_cache_dir():
+    work_dir = os.getcwd()
+    try:
+        # change working directory to avoid import from local folder
+        # during installation process
+        os.chdir(os.path.dirname(sys.executable))
+        import comtypes.client
+        return comtypes.client._code_cache._find_gen_dir()
+    except ImportError:
+        return None
+    finally:
+        os.chdir(work_dir)
 
 def remove_directory(directory, silent):
     """
@@ -60,3 +74,14 @@ def _remove(directory):
     except (PermissionError, OSError) as e:
         print('Error: Cannot remove directory "%s": %s' % (directory, e))
 
+if len(sys.argv) > 1 and "-y" in sys.argv[1:]:
+    silent = True
+else:
+    silent = False
+
+# First iteration may get folder with restricted rights.
+# Second iteration always gets temp cache folder (writable for all).
+directory = get_next_cache_dir()
+removed = remove_directory(directory, silent)
+if removed:
+    directory = get_next_cache_dir()
